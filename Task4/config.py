f6b98af853e58114adb514a8dd27f10218c236b6
Debanjali Biswas <44898589+debanjali05@users.noreply.github.com>
2019-03-05 00:10:33 +0100

    Update config.py

--- Task4/config.py
+++ Task4/config.py
@@ -9,8 +9,9 @@ class Config():
 # assert mode in ["QG", "KBEMBED"]
 
         current_file = Path().resolve().parent
-        parent_of_parent_dir = os.path.join(current_file, '..\\')
-        self.CHECKPOINTS_PATH = os.path.join(str(parent_of_parent_dir)+"\\Task4\\checkpoints\\Triples", "triples")
+        parent_of_parent_dir = os.path.join(current_file, '../')
+        self.CHECKPOINTS_PATH = os.path.join(str(parent_of_parent_dir)+"/Task4/checkpoints/Triples", "triples")
+
 
         # Triples:
         self.TRIPLELENGTH = 3  # (s,p,o) # make 4 to extend to quads
@@ -20,7 +21,7 @@ class Config():
 
         #knowledge base embeddings training params
         #if mode == "KBEMBED":
-        self.KB_EMBED_VOCAB_PATH = str(parent_of_parent_dir)+"\\Task4\\data\\"
+        self.KB_EMBED_VOCAB_PATH = str(parent_of_parent_dir)+"/Task4/data/"
 
 #counting length of entity and properties vocab
 
