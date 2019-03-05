b58f46964cf03c0f27d924a739ce358cf48e9076
chaitaliprabhu <42174470+chaitaliprabhu@users.noreply.github.com>
2019-03-05 19:22:50 +0100

    Update question_generation.py

--- Task4/question_generation.py
+++ Task4/question_generation.py
@@ -35,25 +35,27 @@ def getSuggestiveQuestions(subject,predicate,obj_link,direction):
         predicate_vocab = Dictionary.load_from_text(str(parent_of_parent_dir)+"\\Task4\\data\\predicate.vocab")
         model.restore(sess=sess, path=tf.train.latest_checkpoint(str(parent_of_parent_dir)+"\\Task4\\checkpoints"))
         
-        sub = entity_vocab.token2id[subject]
-        pred = predicate_vocab.token2id[predicate]
-        obj = entity_vocab.token2id[obj_link]
-   
-        encoder_triples_inputs = np.array([[sub,pred,obj]])
+        try:
+            sub = entity_vocab.token2id[subject]
+            pred = predicate_vocab.token2id[predicate]
+            obj = entity_vocab.token2id[object_name[0]]
+       
+            encoder_triples_inputs = np.array([[sub,pred,obj]])
+            
+            if (direction=="forward"):
+                encoder_predicates_direction = [1]
+            else:
+                encoder_predicates_direction = [2]
         
-        if (direction=="forward"):
-            encoder_predicates_direction = [1]
-        else:
-            encoder_predicates_direction = [2]
-    
-        predicted_question =""
-        predicted_ids = model.predict(sess, encoder_triples_inputs=encoder_triples_inputs, encoder_predicates_direction=encoder_predicates_direction)
-   
-        for i in range(len(predicted_ids[0])):
-            x = predicted_ids[0,i]
-            if (x.item(0) == 1):
-                break
-            predicted_question = predicted_question + word_vocab[x.item(0)] + " "
-        predicted_question = predicted_question + "?"
-        return predicted_question
+            predicted_question =""
+            predicted_ids = model.predict(sess, encoder_triples_inputs=encoder_triples_inputs, encoder_predicates_direction=encoder_predicates_direction)
+       
+            for i in range(len(predicted_ids[0])):
+                x = predicted_ids[0,i]
+                predicted_question = predicted_question + word_vocab[x.item(0)] + " "
+                predicated_question = predicted_question + " ?"
+                return str(predicated_question)
+            
+        except Exception:
+             return "Entity or Predicate out of vocabulary"
 
