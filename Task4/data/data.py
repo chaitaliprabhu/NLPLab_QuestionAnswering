e837f67a3891ff343720bc660457d8fd19203b68
chaitaliprabhu <42174470+chaitaliprabhu@users.noreply.github.com>
2019-03-05 22:20:15 +0100

    Update data.py

--- Task4/data/data.py
+++ Task4/data/data.py
@@ -15,13 +15,13 @@ class Data:
         
         # loading vocab
         current_file = Path().resolve()
-        #parent_of_parent_dir = os.path.join(current_file, '../')
-        self.word_vocab = Dictionary.load_from_text(str(current_file)+"/Task4/data/word.vocab")
-        self.entity_vocab = Dictionary.load_from_text(str(current_file)+"/Task4/data/entity.vocab")
-        self.predicate_vocab = Dictionary.load_from_text(str(current_file)+"/Task4/data/predicate.vocab")
+        parent_of_parent_dir = os.path.join(current_file, '../../')
+        self.word_vocab = Dictionary.load_from_text(str(parent_of_parent_dir)+"/Task4/data/word.vocab")
+        self.entity_vocab = Dictionary.load_from_text(str(parent_of_parent_dir)+"/Task4/data/entity.vocab")
+        self.predicate_vocab = Dictionary.load_from_text(str(parent_of_parent_dir)+"/Task4/data/predicate.vocab")
 
         # loading data files names
-        self.datafile =str(current_file)+"/Task4/data/final/train_output.txt"
+        self.datafile =str(parent_of_parent_dir)+"/Task4/data/final/train_output.txt"
         self.data = {}
 
     def read_data(self):
