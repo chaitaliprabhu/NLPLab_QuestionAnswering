98ff0566ff5f40b6ecc029daa804289b630b9f31
chaitaliprabhu <42174470+chaitaliprabhu@users.noreply.github.com>
2019-03-05 19:23:51 +0100

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -4,7 +4,7 @@ Created on Sat Mar  2 15:56:26 2019
 
 @author: Chaitali
 """
-
+import os
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
@@ -13,18 +13,19 @@ from start_main import getAnswerStr, start, getQues
  
  
 class DemoApp(App):
+    current_file = os.getcwd()
+    global output_dir
+    output_dir = current_file+ "\\outputModel"
     def build(self):
         self.box = BoxLayout(orientation='vertical', spacing=10)
         self.txt_question = TextInput()
         self.txt_answer = TextInput(readonly = 'True')
         self.txt_sugg_question = TextInput(readonly = 'True')
-        self.btn_reset = Button(text='Reset', on_press=self.clear_txt)
         self.btn_answer = Button(text='Get Answer', on_press=self.get_ans)
         self.box.add_widget(self.txt_question)
         self.box.add_widget(self.txt_answer)
         self.box.add_widget(self.txt_sugg_question)
         self.box.add_widget(self.btn_answer)
-        self.box.add_widget(self.btn_reset)
         return self.box
          
     def clear_txt(self, instance):
@@ -33,8 +34,9 @@ class DemoApp(App):
         self.txt_sugg_question.text = ''
     
     def get_ans(self, instance):
-        start(self.txt_question.text)
-        self.txt_answer.text = getAnswerStr().strip()
+        global output_dir
+        start(str(self.txt_question.text), output_dir)
+        self.txt_answer.text = str(getAnswerStr()).strip()
         self.txt_sugg_question.text = str(getQues()).strip()
  
 if __name__ == '__main__':
